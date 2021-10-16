7bc59144e53f412b703bb3833ea3e253e937f2f7
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2021-10-15 21:05:32 -0500

    Create addsynctime.sh

---
+++ addsynctime.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+printf "\n\n###########################################\n时间同步脚本\n###########################################\n\n"
+
+wget -P /etc/cuocuo/ -N --no-check-certificate https://raw.githubusercontent.com/SereneWindCoding/Public-Shell/main/synctime.sh
+
+printf "\n\n###########################################\n设置Crontab命令\n###########################################\n\n"
+
+cat >> /var/spool/cron/crontabs/root <<EOF
+30 2 * * * bash /etc/cuocuo/synctime.sh
+EOF
+
+printf "\n\n###########################################\n配置完成\n###########################################\n\n"
