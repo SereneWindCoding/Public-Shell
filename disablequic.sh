2b0c3e87fbd5231ffc607e6aab6532233942cbff
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2022-02-16 07:19:31 -0600

    updates

---
+++ disablequic.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+iptables -A INPUT -p udp --dport 80 -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p udp --dport 443 -j REJECT --reject-with icmp-port-unreachable
+
+echo y|apt-get install iptables-persistent
+
+printf "\n\n###########################################\n部署完成\n###########################################\n\n"
--- logclean.sh
+++ logclean.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #获取当前系统年月日
 start=$(date +%Y%m%d)
@@ -7,3 +7,5 @@ start=$(date +%Y%m%d)
 location="/etc/soga/access_log/"
 
 find $location -mtime +3  -name "*.csv" -exec rm -rf {} \;
+
+printf "\n\n###########################################\n部署完成\n###########################################\n\n"
