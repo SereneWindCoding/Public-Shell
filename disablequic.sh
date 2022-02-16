b75455c97d42d0b92e0c7bc60cc1d44afe6e8bb1
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2022-02-16 07:35:03 -0600

    Update disablequic.sh

--- disablequic.sh
+++ disablequic.sh
@@ -3,7 +3,7 @@
 iptables -A INPUT -p udp --dport 80 -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT -p udp --dport 443 -j REJECT --reject-with icmp-port-unreachable
 
-apt-get install iptables-persistent
+apt-get install iptables-persistent -y
 netfilter-persistent save
 
 printf "\n\n###########################################\n部署完成\n###########################################\n\n"
