1f439e1cc431462bcb33b5d9db131afe90b305c2
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2022-03-02 12:56:12 -0600

    Update addcroncleanlog.sh

--- cleanlog/addcroncleanlog.sh
+++ cleanlog/addcroncleanlog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget -N --no-check-certificate -O /home/cleanlog/cleanlog.sh https://rawgithubusercontent.ninjacloudnetworks.workers.dev/SereneWindCoding/Public-Shell/main/cleanlog/cleanlog.sh
-chmod +x /opt/cleanlog.sh
+chmod +x /home/cleanlog/cleanlog.sh
 
 cat >> /var/spool/cron/crontabs/root <<EOF
 00 6 * * * bash /home/cleanlog/cleanlog.sh
