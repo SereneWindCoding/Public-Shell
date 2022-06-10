6276bf505d18677e4f1b23d6aac6e3a91fcd427c
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2022-06-09 19:39:15 -0500

    Update cuocuo.sh

--- cc/cuocuo.sh
+++ cc/cuocuo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -N --no-check-certificate https://download.renzhe.work/cuocuo -O /usr/bin/cuocuo
+wget -N --no-check-certificate https://github.com/SereneWindCoding/Public-Shell/raw/main/cc/cuocuo -O /usr/bin/cuocuo
 chmod +x /usr/bin/cuocuo
 echo "Finished"
 shutdown -r now
