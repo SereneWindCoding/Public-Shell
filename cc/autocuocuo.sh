b151b0e1b0aa8b6cbb142b30cd6737b13d3da717
SereneWind <56496576+SereneWindCoding@users.noreply.github.com>
2022-06-17 12:36:06 -0500

    cc

---
+++ cc/autocuocuo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -N --no-check-certificate https://github.com/SereneWindCoding/Public-Shell/raw/main/cc/cuocuo -O /usr/bin/cuocuo
+chmod +x /usr/bin/cuocuo
---
+++ cc/cuocuocn.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+chmod +x /usr/bin/cuocuo
+echo "Finished"
+shutdown -r now
