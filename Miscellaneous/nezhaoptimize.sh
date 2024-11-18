c55181f508a6f8ee3a67860f47b538261e449dd5
Wind <dev@renzhecloud.com>
2024-11-17 20:59:19 -0600

    Update nezhaoptimize.sh

--- Miscellaneous/nezhaoptimize.sh
+++ Miscellaneous/nezhaoptimize.sh
@@ -4,10 +4,13 @@
 Green="\033[32m"
 Font="\033[0m"
 
-echo -e ${Green}开始配置${Font}
+echo -e "${Green}开始配置${Font}"
 
-sed -i '/ExecStart/s/$/ --skip-conn --skip-procs/' /etc/systemd/system/nezha-agent.service
+# 修改 ExecStart 行，添加所需参数
+sed -i '/ExecStart/s/$/ --skip-conn --skip-procs --disable-auto-update --disable-force-update --disable-command-execute/' /etc/systemd/system/nezha-agent.service
+
+# 重载 systemd 配置并重启服务
 systemctl daemon-reload
 systemctl restart nezha-agent.service
 
-echo -e ${Green}配置完成${Font}
+echo -e "${Green}配置完成${Font}"
